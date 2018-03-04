85ac0d9b05321b301225e43fef3ada0c2fd8812e
Vedant Singhvi <36934612+vedantsinghvi@users.noreply.github.com>
2018-03-03 19:31:52 -0500

    Add files via upload

---
+++ Project/JavaClass.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JavaClass;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Vedant Singhvi
+ */
+class JavaClass {
+     public static void main(String args[]) {
+         
+         
+         int i=0;
+         int j=1;
+         int k;
+   
+         
+         
+         for(int f=0;f<3;f++){
+          System.out.println(i);
+          System.out.println(j);
+          k=i+j;
+          i=j;
+          j=k;
+         
+         }
+         
+         }
+             
+     }
+    
+    
+    
+    
+    
+}
